b64098fe06689307bc3114eafb2cc1bcd4341148
Thyago Lopes <thyago.brasileiro@solluti.com.br>
2021-06-18 11:01:48 -0300

    validação de formulário

--- app/Http/Requests/Veiculo/VeiculoRequest.php
+++ app/Http/Requests/Veiculo/VeiculoRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Veiculo;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class VeiculoRequest extends FormRequest
 {
@@ -24,7 +26,37 @@ class VeiculoRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'veiculo' => 'required',
+            'marca' => 'required',
+            'ano' => 'required',
+            'descricao' => 'required',
         ];
+    }
+
+     /**
+     * Retorna mensagens personalizadas de validação
+     */
+    public function messages()
+    {
+        return [
+            
+            'veiculo.required' => 'O campo veiculo é obrigatório',
+            'marca.required' => 'O campo marca é obrigatório',
+            'ano.required' => 'O campo ano é obrigatório',
+            'descricao.required' => 'O campo descrição é obrigatório',
+            
+        ];
+    }
+
+    /**
+     * Failed validation disable redirect
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json(['errors' => $validator->errors()], 422)
+        );
     }
 }
--- resources/js/components/veiculo/components/Form.vue
+++ resources/js/components/veiculo/components/Form.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
+    <b-alert :show="error.errors ? true : false" variant="danger" dismissible>
+      <div v-for="(err, index) in error.errors" :key="index">
+        <p>{{err}}</p>
+      </div>
+    </b-alert>
     <b-form @submit.stop.prevent="salvarOuAtualizar">
       <b-form-group label="Veículo">
         <b-form-input v-model="form.veiculo"></b-form-input>
       </b-form-group>
 
       <b-form-group label="Marca">
-          <b-form-select v-model="form.marca">
-              <b-form-select-option v-for="(marca, index) in marcas" :key="index" :value="marca.nome">{{marca.nome}}</b-form-select-option>
-          </b-form-select>
+        <b-form-select v-model="form.marca">
+          <b-form-select-option
+            v-for="(marca, index) in marcas"
+            :key="index"
+            :value="marca.nome"
+            >{{ marca.nome }}</b-form-select-option
+          >
+        </b-form-select>
       </b-form-group>
 
       <b-form-group label="Ano">
@@ -35,7 +45,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 import veiculoMixin from "../../../mixin/veiculo-mixin";
 import { VueEditor } from "vue2-editor";
 export default {
@@ -43,31 +53,32 @@ export default {
   mixins: [veiculoMixin],
   props: ["id"],
   components: {
-      VueEditor
+    VueEditor,
   },
 
   created() {
     if (this.$route.params.id) {
-        this.buscarVeiculo(this.$route.params.id)
+      this.buscarVeiculo(this.$route.params.id);
     }
   },
   data() {
     return {
-        marcas: [
-            {nome: 'Chevrolet '},
-            {nome: 'Hyndai '},
-            {nome: 'Fiat '},
-            {nome: 'Volkswagen'},
-            {nome: 'Honda'},
-            {nome: 'Toyota'},
-            {nome: 'Nissan'},
-        ],
+      error: {},
+      marcas: [
+        { nome: "Chevrolet " },
+        { nome: "Hyndai " },
+        { nome: "Fiat " },
+        { nome: "Volkswagen" },
+        { nome: "Honda" },
+        { nome: "Toyota" },
+        { nome: "Nissan" },
+      ],
       options: [
         { text: "Sim", value: 1 },
         { text: "Não", value: 0 },
       ],
       form: {
-        id: '',
+        id: "",
         veiculo: "",
         marca: "",
         ano: "",
@@ -79,11 +90,11 @@ export default {
 
   methods: {
     salvarOuAtualizar() {
-        if (this.form.id) {
-            this.atualizarVeiculo()
-        }
+      if (this.form.id) {
+        this.atualizarVeiculo();
+      }
 
-        this.salvarVeiculo()
+      this.salvarVeiculo();
     },
     salvarVeiculo() {
       this.$axios
@@ -94,20 +105,29 @@ export default {
             text: "Veículo salvo com sucesso.",
             icon: "success",
           });
-          this.$router.push({name: 'veiculos'})
-          
+          this.$router.push({ name: "veiculos" });
         })
-        .catch(() => {
-          this.$swal.fire({
-            title: "Erro",
-            text: "Não foi possível salvar o veículo.",
-            icon: "error",
-          });
+       .catch((error) => {
+          console.log(error.response)
+          if (error.response.status == 422) {
+            this.error = error.response.data
+            this.$swal.fire({
+              title: "Erro",
+              text:  'Preencha todos os campos obrigatórios',
+              icon: "error",
+            });
+          } else {
+            this.$swal.fire({
+              title: "Erro",
+              text: "Não foi possível salvar o veículo.",
+              icon: "error",
+            });
+          }
         });
     },
 
     atualizarVeiculo() {
-        this.$axios
+      this.$axios
         .put(`api/veiculos/${this.form.id}`, this.form)
         .then(() => {
           this.$swal.fire({
@@ -115,15 +135,23 @@ export default {
             text: "Veículo salvo com sucesso.",
             icon: "success",
           });
-          this.$router.push({name: 'veiculos'})
-          
+          this.$router.push({ name: "veiculos" });
         })
-        .catch(() => {
-          this.$swal.fire({
-            title: "Erro",
-            text: "Não foi possível salvar o veículo.",
-            icon: "error",
-          });
+        .catch((error) => {
+          if (error.response.status == 422) {
+            this.error = error.response.data
+            this.$swal.fire({
+              title: "Erro",
+              text: 'Preencha todos os campos obrigatórios',
+              icon: "error",
+            });
+          } else {
+            this.$swal.fire({
+              title: "Erro",
+              text: "Não foi possível salvar o veículo.",
+              icon: "error",
+            });
+          }
         });
     },
 
